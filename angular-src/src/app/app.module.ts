9812c898b4c939768b41f29b70a08eb278b9c3e7
Tristan Hull <soldier@kenpokicks.com>
2019-03-24 19:20:29 -0700

    initial template setup

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule
